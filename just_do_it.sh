d67c5742e4f5ef74710a3406b65543c04cc3a781
Manisha Pandey <37845282+Manisha53@users.noreply.github.com>
2025-06-16 00:01:03 +0530

    Update just_do_it.sh

--- just_do_it.sh
+++ just_do_it.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-
-for var in java python c++ c dotnet ruby
-do
-        echo "Inside the for loop"
-        echo "###################"
-        echo "Value of var is $var."
-        echo "##################"
-        date
-done
-[root@scriptbox scripts]# cat just_do_it.sh 
-#!/bin/bash
 echo "Bash version ${BASH_VERSION}"
 for i in {0..10..2}
 do
